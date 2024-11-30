f3e44da9ce6fa6aa25db7993b1a2f678adbb78b6
ViperChaos <144017034+Nathanial01@users.noreply.github.com>
2024-11-30 14:15:27 +0100

    Update 2024_11_26_000000_create_chat_histories_table.php

--- src/Database/migrations/2024_11_26_000000_create_chat_histories_table.php
+++ src/Database/migrations/2024_11_26_000000_create_chat_histories_table.php
@@ -1,5 +1,5 @@
 <?php
-namespace Cyrox\Chatbot\Database\migrations;
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
